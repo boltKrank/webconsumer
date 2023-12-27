2c391bff7b8a59462852ce72bc74aa8c99b9adff
simon.anderson <simon.anderson@datadoghq.com>
2023-12-28 08:48:28 +1100

    Updated deploy script

--- update_deployment.sh
+++ update_deployment.sh
@@ -6,5 +6,12 @@ docker tag webconsumer:0.1 simonanderson/webconsumer
 docker push simonanderson/webconsumer
 kubectl apply -f deployment.yaml
 kubectl get all
-sleep 20
-kubectl port-forward svc/webconsumer 8080:8080
+sleep 15
+minikube service webconsumer --url
+
+
+# kubectl port-forward svc/webconsumer 8080:8080
+# kubectl expose deployment webconsumer --port=8080
+# kubectl create deployment webconsumer --image=simonanderson/webconsumer --dry-run=client -o=yaml > deployment.yaml
+# echo --- >> deployment.yaml
+# kubectl create service clusterip webconsumer --tcp=8080:8080 --dry-run=client -o=yaml >> deployment.yaml
