eb2c145a308373e65f97224319eaaef99760c2be
simon.anderson <simon.anderson@datadoghq.com>
2023-12-28 08:33:22 +1100

    Updated script

--- update_deployment.sh
+++ update_deployment.sh
@@ -6,3 +6,5 @@ docker tag webconsumer:0.1 simonanderson/webconsumer
 docker push simonanderson/webconsumer
 kubectl apply -f deployment.yaml
 kubectl get all
+sleep 20
+kubectl port-forward svc/webconsumer 8080:8080
