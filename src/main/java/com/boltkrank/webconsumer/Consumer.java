4d2759102e43297a5b8bc11d9e6ca3fe09d95211
simon.anderson <simon.anderson@datadoghq.com>
2023-12-29 06:41:53 +1100

    Basic input handling, no processing

--- src/main/java/com/boltkrank/webconsumer/Consumer.java
+++ src/main/java/com/boltkrank/webconsumer/Consumer.java
@@ -2,27 +2,63 @@ package com.boltkrank.webconsumer;
 
 public class Consumer {
     
-    private int iterations;
-    private int level;
+    private String iterations;
+    private String level;
+    private boolean running;
 
-    public int getIterations(){
+    public Consumer(){
+        this.running = false;
+    }
+
+    public String getIterations(){
         return iterations;
     }
 
-    public void setIterations(int iterations){
+    public void setIterations(String iterations){
         this.iterations = iterations;
     }
 
-    public int getLevel(){
+    public String getLevel(){
         return level;
     }
 
-    public void setLevel(int level){
+    public void setLevel(String level){
         this.level = level;
     }
 
+    public void setRunning(boolean running){
+        this.running = running;
+    }
+
+    public boolean getRunning(){
+        return running;
+    }
+
+    /**
+     * 
+     */
     public void runConsumption(){
-        
+
+        setRunning(true);
+        System.out.println("Running");
+
+        int tmp_iter = Integer.getInteger(getIterations());
+        int tmp_level = 0;
+        if (getLevel().equals("low")){
+            tmp_level = 1;
+        }
+        else if (getLevel().equals("medium")){
+            tmp_level = 2;
+        }
+        else {
+            tmp_level = 3;
+        }
+
+        System.out.println("Iterations: " + tmp_iter + "\nLevel: " + tmp_level);
+
+        setRunning(false);
+        System.out.println("Finished Running");
+
     }
 
 }
--- src/main/java/com/boltkrank/webconsumer/WebconsumerRootController.java
+++ src/main/java/com/boltkrank/webconsumer/WebconsumerRootController.java
@@ -1,6 +1,8 @@
 package com.boltkrank.webconsumer;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
@@ -11,7 +13,20 @@ public class WebconsumerRootController {
     @GetMapping("/")
 	public String consumerForm(Model model) {
 		model.addAttribute("consumer", new Consumer());
-		return "webconsumer";
+		return "webconsumer_input";
+	}
+
+    @PostMapping("/")
+	public String consumerSubmit(@ModelAttribute Consumer consumer, Model model) {
+
+        System.out.println("Level: " + consumer.getLevel());
+
+        System.out.println("Iterations: " + consumer.getIterations());
+
+        consumer.runConsumption();
+
+	  model.addAttribute("consumer", consumer);
+	  return "webconsumer_output";
 	}
 
 }
