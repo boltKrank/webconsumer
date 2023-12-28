f373bcb5901597a2d3c8ebccccc86a0419075ddb
simon.anderson <simon.anderson@datadoghq.com>
2023-12-28 13:09:06 +1100

    Updated landing HTML

---
+++ src/main/java/com/boltkrank/webconsumer/Consumer.java
@@ -0,0 +1,28 @@
+package com.boltkrank.webconsumer;
+
+public class Consumer {
+    
+    private int iterations;
+    private int level;
+
+    public int getIterations(){
+        return iterations;
+    }
+
+    public void setIterations(int iterations){
+        this.iterations = iterations;
+    }
+
+    public int getLevel(){
+        return level;
+    }
+
+    public void setLevel(int level){
+        this.level = level;
+    }
+
+    public void runConsumption(){
+        
+    }
+
+}
--- src/main/java/com/boltkrank/webconsumer/WebconsumerRootController.java
+++ src/main/java/com/boltkrank/webconsumer/WebconsumerRootController.java
@@ -2,13 +2,16 @@ package com.boltkrank.webconsumer;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+
 
 @RestController
 public class WebconsumerRootController {
     
     @GetMapping("/")
-    public String index() {
-        return "Hello from Web Consumer";
-    }
+	public String revHashForm(Model model) {
+		model.addAttribute("consumer", new Consumer());
+		return "webconsumer";
+	}
 
 }
