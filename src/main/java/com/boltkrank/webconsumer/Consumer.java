5906e510fa3c332c1040a56992ed17e4e2d30c4b
Simon Anderson <simon.anderson111@gmail.com>
2024-01-02 16:33:22 +1100

    Added sorting algorithms

--- src/main/java/com/boltkrank/webconsumer/Consumer.java
+++ src/main/java/com/boltkrank/webconsumer/Consumer.java
@@ -1,5 +1,7 @@
 package com.boltkrank.webconsumer;
 
+import java.util.Random;
+
 public class Consumer {
     
     private String iterations;
@@ -43,22 +45,127 @@ public class Consumer {
         System.out.println("Running");
 
         int tmp_iter = Integer.getInteger(getIterations());
-        int tmp_level = 0;
-        if (getLevel().equals("low")){
-            tmp_level = 1;
-        }
-        else if (getLevel().equals("medium")){
-            tmp_level = 2;
-        }
-        else {
-            tmp_level = 3;
+
+        //Create Array and fill will random values
+        int[] numberList = new int[tmp_iter];
+        
+        //Create random number generator.
+        Random ran = new Random();
+
+        for(int i=0; i<numberList.length; i++){
+            numberList[i] = ran.nextInt(1000);
         }
 
-        System.out.println("Iterations: " + tmp_iter + "\nLevel: " + tmp_level);
+        if (getLevel().equals("low")){
+
+            System.out.println("Array before insertion sort:");
+            for(int i=0; i<numberList.length;i++){
+                System.out.println(" "+numberList[i]+" ");
+            }
+            numberList = insertionSort(numberList);
+            
+            System.out.println("Array after insertion sort:");
+            for(int i=0; i<numberList.length;i++){
+                System.out.println(" "+numberList[i]+" ");
+            }
+
+        }
+        else if (getLevel().equals("medium")){
+
+            
+            System.out.println("Array before bubble sort:");
+            for(int i=0; i<numberList.length;i++){
+                System.out.println(" "+numberList[i]+" ");
+            }
+            numberList = bubbleSort(numberList);
+
+            System.out.println("Array after bubble sort:");
+            for(int i=0; i<numberList.length;i++){
+                System.out.println(" "+numberList[i]+" ");
+            }
+
+
+        }
+        else {
+
+            System.out.println("Array before selection sort:");
+            for(int i=0; i<numberList.length;i++){
+                System.out.println(" "+numberList[i]+" ");
+            }
+                        
+            numberList = selectionSort(numberList);
+
+            
+            System.out.println("Array after selection sort:");
+            for(int i=0; i<numberList.length;i++){
+                System.out.println(" "+numberList[i]+" ");
+            }
+
+        }
+
+        System.out.println("Iterations: " + tmp_iter + "\nLevel: " + getLevel());
 
         setRunning(false);
         System.out.println("Finished Running");
 
+    }
+
+    int[] insertionSort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+ 
+            /* Move elements of arr[0..i-1], that are
+               greater than key, to one position ahead
+               of their current position */
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+
+        return arr;
+    }
+
+    int[] bubbleSort(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i = 0; i < n - 1; i++) 
+            for (int j = 0; j < n - i - 1; j++) 
+                if (arr[j] > arr[j + 1]) { 
+                    // swap temp and arr[i] 
+                    int temp = arr[j]; 
+                    arr[j] = arr[j + 1]; 
+                    arr[j + 1] = temp; 
+                } 
+
+        return arr;
+    } 
+    
+    int[] selectionSort(int arr[])
+    {
+        int n = arr.length;
+ 
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n-1; i++)
+        {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+ 
+            // Swap the found minimum element with the first
+            // element
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+
+        return arr;
     }
 
 }
